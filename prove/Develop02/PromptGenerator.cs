b0305f28d8b0907e89f9fb6f20648bd3e99711b6
sgoodmansen <sgoodmansen@gmail.com>
2026-05-12 11:57:42 -0600

    Add more prompts

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -6,7 +6,13 @@ public class PromptGenerator
         "What made you smile today?",
         "What did you learn today?",
         "What was the nicest thing you did today?",
-        "What was the best food you ate today?"
+        "What was the best food you ate today?",
+        "What is something you are proud of, and why?",
+        "What are you feeling right now—and why?",
+        "What is your favorite thing about your personality?",
+        "What are three goals you want to achieve this year?",
+        "What is a skill you want to master in the next 6 months?",
+        "What is your favorite thing in your room that brings you joy?"
     };
 
     Random randomGen = new Random();
